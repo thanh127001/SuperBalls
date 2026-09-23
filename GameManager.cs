87fb665102d9c2ed222954b20a6dfedfafdc8e44
thanh127001 <54224744+thanh127001@users.noreply.github.com>
2026-09-23 12:03:05 +0700

    Add editor game state test mode

--- GameManager.cs
+++ GameManager.cs
@@ -134,6 +134,55 @@ public class GameManager : MonoBehaviour
 
     /*
      * ========================================
+     * EDITOR TEST
+     * ========================================
+     */
+#if UNITY_EDITOR
+    public enum EditorTestMode
+    {
+        Normal,
+        LevelCompleted,
+        GameCompleted
+    }
+
+    [HideInInspector]
+    [SerializeField]
+    private EditorTestMode editorTestMode =
+        EditorTestMode.Normal;
+
+    public EditorTestMode CurrentEditorTestMode =>
+        editorTestMode;
+
+    public void SetEditorTestMode(
+        EditorTestMode testMode)
+    {
+        if (editorTestMode == testMode)
+        {
+            return;
+        }
+
+        editorTestMode = testMode;
+
+        if (!Application.isPlaying)
+        {
+            return;
+        }
+
+        switch (editorTestMode)
+        {
+            case EditorTestMode.LevelCompleted:
+                CompleteLevel();
+                break;
+
+            case EditorTestMode.GameCompleted:
+                CompleteGame();
+                break;
+        }
+    }
+#endif
+
+    /*
+     * ========================================
      * GAME EVENTS
      * ========================================
      */
