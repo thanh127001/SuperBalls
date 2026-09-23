a958d159b2869f705e9832632fc59e723904552d
thanh127001 <54224744+thanh127001@users.noreply.github.com>
2026-09-23 12:53:17 +0700

    Keep PurchaseManager scene-owned

--- PurchaseManager.cs
+++ PurchaseManager.cs
@@ -67,10 +67,6 @@ public class PurchaseManager : MonoBehaviour
             return;
         }
 
-        DontDestroyOnLoad(
-            gameObject
-        );
-
         InitializePurchase();
     }
 
